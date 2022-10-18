f0c17c85a947dc83b8acb1bf21068f41d8397e2d
designhappily <65686852+shivakalai@users.noreply.github.com>
2022-10-18 09:29:53 +0200

    React basic concepts revised

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <div className="App">
+      <h1 style= {{textAlign: "center"}}>Hello </h1>
+
+      <button className="clickBtn">Click Me</button>
+    </div>
+  );
+}
+
+export default App;
